0a86e8760419d8d48566002d6e17254c408e3ac1
harrison-jaewon-lee <142871947+harrison-jaewon-lee@users.noreply.github.com>
2023-08-22 15:56:41 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reacjs-tree-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-j1iuv1)
---
+++ src/App.js
@@ -0,0 +1,155 @@
+import React from 'react';
+import pq from 'pqgrid';
+import './style.css';
+
+const rGridColData = [
+  {
+    id: 0,
+    name: 'C',
+    date: '02/19/2010',
+    children: [
+      {
+        id: 1,
+        name: 'my.ini',
+        size: 9,
+        date: '05/18/2013',
+      },
+      {
+        id: 2,
+        name: 'Program Files',
+        date: '03/21/2016',
+        children: [
+          {
+            id: 21,
+            name: 'Java',
+            date: '01/13/2010',
+            pq_close: false,
+            children: [
+              {
+                id: 211,
+                name: 'java.exe',
+                size: 142,
+                date: '01/10/2015',
+                pq_tree_cb: true, //to check the checkbox.
+              },
+              {
+                id: 212,
+                name: 'license.txt',
+                size: 5,
+                date: '01/01/2016',
+              },
+            ],
+          },
+          {
+            id: 22,
+            name: 'Android',
+            date: '01/13/2010',
+            pq_close: false,
+            children: [
+              {
+                id: 221,
+                name: 'license.txt',
+                size: 9,
+                date: '05/18/2013',
+                pq_tree_cb: true, //to check the checkbox.
+              },
+              {
+                id: 222,
+                name: 'Android SDK',
+                size: '5',
+                date: '02/25/2013',
+              },
+              {
+                id: 223,
+                name: 'Chrome',
+                size: '5',
+                date: '03/25/2013',
+              },
+              {
+                id: 224,
+                name: 'firefox',
+                size: '3',
+                date: '03/26/2013',
+              },
+            ],
+          },
+          {
+            id: 23,
+            name: 'my.ini',
+            size: '9',
+            date: '05/18/2013',
+          },
+        ],
+      },
+      {
+        id: 4,
+        name: 'my.ini',
+        size: 9,
+        date: '05/18/2013',
+      },
+    ],
+  },
+];
+
+class PqGrid extends React.Component {
+  gridRef = React.createRef();
+  componentDidMount() {
+    this.options = this.props.option;
+    this.grid = pq.grid(this.gridRef.current, this.options);
+    this.grid.Tree().option({ dataIndx: 'name', cascade: true });
+  }
+  componentDidUpdate(prevProps) {
+    Object.assign(this.options, this.props.option);
+  }
+  render() {
+    return <div ref={this.gridRef}></div>;
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.rGridCol = [
+      { dataIndx: 'pq_tree_cb', width: 100, hidden: true },
+      { dataIndx: 'name', width: 200, title: 'Name' },
+      { dataIndx: 'size', title: 'Size', width: 100, dataType: 'float' },
+      {
+        dataIndx: 'date',
+        title: 'Modified Date',
+        width: 100,
+        dataType: 'date',
+      },
+    ];
+
+    this.rGridOption = {
+      showTitle: false,
+      reactive: true,
+      locale: 'en',
+      animModel: { on: true },
+      collapsible: { toggled: true },
+      rowHt: 24,
+      height: '400',
+      columnTemplate: { width: 100 },
+
+      colModel: this.rGridCol,
+      dataModel: { data: rGridColData },
+    };
+
+    this.state = {
+      gridRight: this.rGridOption,
+    };
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="grid-right">
+          <PqGrid option={this.state.gridRight} />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;

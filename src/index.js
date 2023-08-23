2aaa05200a204c35fbee14dd2fa41f0cc5013d12
harrison-jaewon-lee <142871947+harrison-jaewon-lee@users.noreply.github.com>
2023-08-24 00:43:49 +0900

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+//import App from './App';
 //import App from './App_my';
-//import App from './App_disp';
+import App from './App_disp';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
